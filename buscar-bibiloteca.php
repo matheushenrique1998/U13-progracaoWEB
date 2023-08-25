5342224bbc1179f06169f1bf209592ed997106ba
matheushenrique1998 <134648358+matheushenrique1998@users.noreply.github.com>
2023-08-25 14:47:45 -0300

    m

---
+++ buscar-bibiloteca.php
@@ -0,0 +1,16 @@
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>pesquisar aluno</title>
+    </head>
+    <body>
+        <h1>pesquisar aluno </h1>
+        <form action="endereco.php" method="post">
+            <fieldset>
+                <legend>digite o nome:</legend>
+                <input name="nome" type="text" placeholder="0000-000">
+                <button type="submit">pesquisar</button>
+            </fieldset>
+        </form>
+    </body>
+</html>
