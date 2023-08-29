0fc4dcd95bc79e9fa5badf6c1df2f0b99f58469b
matheushenrique1998 <134648358+matheushenrique1998@users.noreply.github.com>
2023-08-29 16:55:03 -0300

    o

---
+++ index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>bootstrap</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+</head>
+</head>
+
+<body>
+    <div class="container">
+        <div class="container text-bg-primary p-1 m-2 text-center  rounded-5 ">
+            <h1>aula de bootstrap</h1>
+        </div>
+        <div class="row">
+            <div class="bg-danger  col-12 col-md-4 ">
+                <h2>coluna 1</h2>
+            </div>
+            <div class="bg-warning col-6 col-md-4">
+                <h2>coluna 2</h2>
+            </div>
+            <div class="bg-success col-6 col-md-4">
+                <h2>coluna 3</h2>
+            </div>
+
+            <div class="row">
+            <div class=" bg-primary-subtle col-12 col-md-12">
+                <h2>coluna 4</h2>
+            </div>
+            <div class=" bg-secondary-subtle col-12 col-md-6 ">
+                <h2>coluna 5</h2>
+            </div>
+            <div class=" bg-success-subtle col-12 col-md-6 d-none d-md-block ">
+                <h2>coluna 6</h2>
+            </div>
+            <div class="bg-info-subtle col-12 col-md-12  ">
+                <h2>coluna 7</h2>
+            </div>
+
+
+
+        </div>
+    </div>
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+</body>
+
+</html>
